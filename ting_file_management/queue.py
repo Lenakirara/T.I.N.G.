7ef5ead795818eb2091819fd5fc651b50f550d2e
Ghislaine <glenox79@gmail.com>
2022-03-10 15:48:52 -0300

    feature: implementing queue to store files

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,31 @@
+# pegando ideia de queue feita na aula ao vivo:
+# https://github.com/tryber/sd-011-live-lectures/blob/lecture-37.2/queue.py
+# estudando tbm um pouco de fila (queue) em:
+# https://www.youtube.com/watch?v=tiee9D54tE0
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui """
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
+
+    # metodo para saber se a lista está vazia
+    def is_empty(self):
+        return self._data == []
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        # adiciona valor a fila/lista
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        # removendo dado que está mais tempo na fila
+        if not self.is_empty():
+            return self._data.pop(0)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        # buscando um indice valido
+        # se indice for >= que 0 ou indice <= que tamanho dessa lista
+        if 0 <= index <= len(self._data):
+            return self._data[index]
+        raise IndexError
